13430f0c1c06e670e46b3e6b06c7708fe4267e19
Trang Nguyen <trangntt.016@gmail.com>
2023-09-17 17:40:44 -0400

    first commit

---
+++ src/main/java/com/vn/anphuc/AnphucApplication.java
@@ -0,0 +1,13 @@
+package com.vn.anphuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnphucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnphucApplication.class, args);
+    }
+
+}
